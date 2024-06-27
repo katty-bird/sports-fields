87729b1500c7d0c1daba643c912121983bf376c0
Hien Tran <hien.tptran@gmail.com>
2024-06-27 15:21:24 +0200

    Adjust element position

--- src/react/InfoPage.jsx
+++ src/react/InfoPage.jsx
@@ -1,12 +1,16 @@
 import * as React from 'react'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import DirectionsIcon from '@mui/icons-material/Directions'
 import {
   Rating,
   Card,
   CardHeader,
   IconButton,
   CardActionArea,
-  Grid
+  Grid,
+  Button,
+  CardActions,
+  CardContent
 } from '@mui/material'
 import CardMedia from '@mui/material/CardMedia'
 import InfoTabs from './InfoTabs'
@@ -18,28 +22,37 @@ const InfoPage = () => (
     direction="column"
     alignItems="center"
     justify="center"
-    style={{ minHeight: '100vh' }}
   >
-    <Card sx={{ maxWidth: 500 }} centered>
-      <IconButton id="back-button" aria-label="back" size="large">
-        <ChevronLeftIcon />
-      </IconButton>
+    <Card sx={{ minWidth: 600, p: 5 }}>
+      <CardActions>
+        <IconButton id="back-button" aria-label="back" size="large">
+          <ChevronLeftIcon />
+        </IconButton>
+      </CardActions>
+
       <CardHeader
+        id="info-header"
         title="Sample Football Field"
         subheader="Hauptstraße 17 12529 Berlin"
         action={
           <Rating value={4} readOnly />
         }
       />
-      <CardActionArea>
+      <CardActions>
+        <Button id="directions-button" variant="contained" startIcon={<DirectionsIcon />}>
+          Get Directions
+        </Button>
+      </CardActions>
+      <CardContent>
         <CardMedia
+          id="info-media"
           component="img"
           height="200"
-          image="./assets/react-logos/react-logo-1.png"
+          image="../assets/placeholder.png"
           alt="placeholder"
         />
-      </CardActionArea>
-      <InfoTabs />
+        <InfoTabs id="info-tabs" />
+      </CardContent>
     </Card>
   </Grid>
 )
