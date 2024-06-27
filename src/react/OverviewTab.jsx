82ac78f621c5e3757ff77d3c88f922356b17c805
Hien Tran <hien.tptran@gmail.com>
2024-06-27 11:33:45 +0200

    Add sample code

---
+++ src/react/OverviewTab.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const OverviewTab = () => (
+  <div>Overview tab</div>
+)
+
+export default OverviewTab
---
+++ src/react/ReviewsTab.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const ReviewsTab = () => (
+  <div>Reviews tab</div>
+)
+
+export default ReviewsTab
