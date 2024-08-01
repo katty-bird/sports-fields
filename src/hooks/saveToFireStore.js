2d57b7b79ba84b3fab2d8bf824537f992e199b58
Hien Tran <hien.tptran@gmail.com>
2024-08-01 15:15:26 +0200

    add hook to save place details into firestore db

---
+++ src/hooks/saveToFireStore.js
@@ -0,0 +1,30 @@
+import { getApp } from 'firebase/app'
+import {
+  getFirestore, doc, setDoc
+} from 'firebase/firestore'
+
+const saveToFireStore = async (
+  {
+    placeID,
+    placeName,
+    placeAddress,
+    openingHours,
+    isOpen,
+    placePhoto,
+    placeReviews,
+    placeRating
+  }
+) => {
+  const db = getFirestore(getApp())
+  await setDoc(doc(db, 'sport-fields', placeID), {
+    name: placeName,
+    address: placeAddress,
+    opening_hours: openingHours,
+    open_now: isOpen,
+    photo: placePhoto,
+    reviews: placeReviews,
+    rating: placeRating
+  })
+}
+
+export default saveToFireStore
